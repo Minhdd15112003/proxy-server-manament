e57b1ce73638c1b2647ea42b7d9a4bc601ff0734
Minhdd15112003 <minhdd15112003@gmail.com>
2024-12-08 10:14:19 +0700

    update controller domain

--- src/controllers/proxy.controller.js
+++ src/controllers/proxy.controller.js
@@ -104,6 +104,86 @@ class proxyController {
       });
   }
 
+  async postblockDomain(req, res) {
+    const { domainName, subDomainName, blockWhiteStatus, domainLable } =
+      req.body;
+
+    // Kiểm tra sự tồn tại của domain
+    const existDomain = await domainModal
+      .findById(req.params.id)
+      .then((data) => {
+        return data.domain.filter((domain) => domain.domainName === domainName);
+      });
+
+    // Nếu domain đã tồn tại, trả về lỗi
+    if (existDomain.length > 0) {
+      return res.status(500).json({ message: "Tên miền đã tồn tại" });
+    }
+
+    try {
+      // Cập nhật thông tin domain và subdomains
+      await domainModal.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: {
+            domain: {
+              domainLable: domainLable,
+              domainName: domainName,
+              subdomain: subDomainName,
+              blockWhiteStatus: blockWhiteStatus,
+            },
+          },
+        }
+      );
+
+      // Trả về thông báo thành công
+      res.json({ message: "Thêm tên miền thành công" });
+    } catch (err) {
+      console.error("Lỗi khi thêm tên miền:", err);
+      res.status(500).json({ message: "Thêm tên miền không thành công" });
+    }
+  }
+
+  async updateBlockDomain(req, res) {
+    const { domainName, subDomainName, blockWhiteStatus, domainLable } =
+      req.body;
+    // // Kiểm tra sự tồn tại của domain
+    // const existDomain = await domainModal
+    //   .findById(req.params.id)
+    //   .then((data) => {
+    //     return data.domain.filter((domain) => domain.domainName === domainName);
+    //   });
+
+    // if (existDomain.length > 0) {
+    //   return res.status(500).json({ message: "Tên miền đã tồn tại" });
+    // }
+
+    await domainModal
+      .updateOne(
+        { "domain._id": req.params.domainid },
+        {
+          $set: {
+            "domain.$.domainLable": domainLable,
+            "domain.$.domainName": domainName,
+            "domain.$.subdomain": subDomainName,
+            "domain.$.blockWhiteStatus": blockWhiteStatus,
+          },
+        },
+        { new: true }
+      )
+      .then((data) => {
+        res.json({ message: "Cập nhật tên miền thành công", data });
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({
+            message: "Cập nhật tên miền không thành công",
+            err: err.message,
+          });
+      });
+  }
+
   async postDomain(req, res) {
     const domainName = req.body.domainName;
     const statusDomain = req.body.statusDomain;
@@ -144,7 +224,7 @@ class proxyController {
       });
   }
 
-  async updateDomain(req, res) {
+  async updateStatusDomain(req, res) {
     const newStatus = req.body.status;
     const newTrash = req.body.blockWhiteStatus;
 
--- src/routes/proxy.route.js
+++ src/routes/proxy.route.js
@@ -2,12 +2,24 @@ var express = require("express");
 const proxyController = require("../controllers/proxy.controller");
 var proxyRouter = express.Router();
 
+proxyRouter.get("/", (req, res) => {
+  res.redirect("/ip");
+});
+
 proxyRouter.get("/ip", proxyController.findIp);
 proxyRouter.patch("/updateIp/:id", proxyController.updateIp);
 
 proxyRouter.get("/ip/:id/domain", proxyController.findDomain);
-proxyRouter.patch("/updateDomain/:id", proxyController.updateDomain);
+proxyRouter.patch(
+  "/updateStatusDomain/:id",
+  proxyController.updateStatusDomain
+);
 proxyRouter.patch("/postDomain/:id", proxyController.postDomain);
+proxyRouter.patch("/postblockDomain/:id", proxyController.postblockDomain);
+proxyRouter.patch(
+  "/updateblockDomain/:domainid",
+  proxyController.updateBlockDomain
+);
 proxyRouter.patch("/updateAllDomains/:id", proxyController.updateAllDomains);
 proxyRouter.get("/blockWhileList/:id", proxyController.findBlockWhite);
 
